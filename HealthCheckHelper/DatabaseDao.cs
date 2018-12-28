1c48e0137d107d9cc4431ac7c033c3a6ca914ce5
Charlie Besalke <Charles.Besalke@vu.com>
2018-12-28 14:48:12 -0600

    Adding WIP for CreditVendorService

---
+++ HealthCheckHelper/DatabaseDao.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HealthCheckHelper
+{
+    public abstract class DatabaseDao : IHealthCheck
+    {
+        protected readonly string ConnectionString;
+
+        protected DatabaseDao(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync(cancellationToken);
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    return new HealthCheckResult(HealthStatus.Unhealthy, ex.Message);
+                }
+            }
+
+            return new HealthCheckResult(HealthStatus.Healthy, "Sucessful connection to DB established");
+        }
+    }
+}
---
+++ HealthCheckHelper/WebDao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HealthCheckHelper
+{
+    public abstract class WebDao : IHealthCheck
+    {
+        protected string HealthCheckUri { get; set; }
+        protected string BaseUri { get; set; }
+        protected HttpClient Client { get; set; }
+
+        public WebDao(string baseUri, string healthCheckUri)
+        {
+            BaseUri = baseUri;
+            HealthCheckUri = healthCheckUri;
+            Client = new HttpClient()
+            {
+                BaseAddress = new Uri(BaseUri)
+            };
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            var result = await Client.GetAsync(HealthCheckUri, cancellationToken);
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                return new HealthCheckResult(HealthStatus.Healthy);
+            }
+
+            return new HealthCheckResult(HealthStatus.Unhealthy);
+        }
+    }
+}
